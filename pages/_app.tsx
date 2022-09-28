3a257b16bf7c4bdee23b1c80e4bedf07192ec1b1
L2HYUNN <dhl9810@naver.com>
2022-09-28 16:16:49 +0900

    #2.0 Patterns

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <NavBar />
+    <Layout>
       <Component {...pageProps} />
       <style jsx global>{`
         a {
           color: white;
         }
       `}</style>
-    </>
+    </Layout>
   );
 }
 // #1.7 Recap
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 const About = () => {
   return (
     <div>
+      <Seo title="About" />
       <h1>About!</h1>
     </div>
   );
