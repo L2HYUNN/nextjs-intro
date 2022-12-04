53b68480d2d1e4a0d84f3bce4d6ca3f9cc89d7fb
L2HYUNN <dhl9810@naver.com>
2022-12-04 13:22:41 +0900

    #2.0 Patterns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
 
 export default function Home() {
   return (
     <div>
+      <Seo title="Home" />
       <h1 className="active">Hello</h1>
     </div>
   );
